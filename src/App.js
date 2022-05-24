f261508c42e86aa675b77a429fff98a2cde8b5e8
RanjanDeveloper <ranjanrocket4@gmail.com>
2022-05-25 00:27:38 +0530

    25-05-22 expe item

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-<<<<<<< HEAD
-import ExpenseItem from "./components/ExpenseItem";
-=======
-import ExpenseItem from './components/ExpenseItem'
->>>>>>> 42d23b8208ff5b3d488c7b5a0ae84664a844bd0c
-
+import Expenses from "./components/Expenses";
+const expenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 function App() {
   return (
-    <>
-      <ExpenseItem />
-    </>
+  <Expenses items={expenses} />
   );
 }
 
